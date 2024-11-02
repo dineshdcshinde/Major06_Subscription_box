1009bf49b2637c8e9677bbbc6525867691c88de0
Dinesh_DC <dineshshinde81563@gmail.com>
2024-11-02 12:21:52 +0530

    Rest foot

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let annual = document.querySelector(".annual");
 let sessional = document.querySelector(".sessional");
 
-annual.addEventListener("click",()=>{
-    annual.style.border= " 10px solid #fb6a66";
-    sessional.style.border=" 10px solid transparent";
-    console.log("i from annual")
+annual.addEventListener("click", () => {
+  annual.style.border = " 10px solid #fb6a66";
+  sessional.style.border = " 10px solid transparent";
+  console.log("i from annual");
 });
 
-sessional.addEventListener("click",()=>{
-    annual.style.border= " 10px solid transparent";
-    sessional.style.border="10px solid #fb6a66";
-    console.log("i from sessioanl")
-})
+sessional.addEventListener("click", () => {
+  annual.style.border = " 10px solid transparent";
+  sessional.style.border = "10px solid #fb6a66";
+  console.log("i from sessioanl");
+});
